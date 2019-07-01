7f33cda170ab7666e040a186eb6cf5e95496ecea
letshare <brucecai2012@gmail.com>
2019-07-01 09:18:31 +0800

    feat: add codes&demo

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # socket.io-study
 socket.io 深入学习demo
 
+## 如何运行
+
+### 安装依赖
+```shell
+npm install
+```
+### 运行
+```shell
+npm run start
+```
+
 ## docs
 
 * [client 深入学习](./docs/socket.io.client学习.md)
---
+++ src/server/app.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+});
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    return next();
+  });
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
